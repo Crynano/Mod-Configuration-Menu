0ab2dc5c45a5c3fa42a69f043a4ae58fadbfd315
Crynano <pa.crysis@gmail.com>
2025-10-09 15:56:39 +0200

    Added TextBoxConfig constructor.

---
+++ src/Implementations/TextBoxConfig.cs
@@ -0,0 +1,21 @@
+﻿using ModConfigMenu.Contracts;
+using System.Collections.Generic;
+
+namespace ModConfigMenu.Implementations
+{
+    public class TextBoxConfig : BaseConfig
+    {
+        public TextBoxConfig(string key, string value, string header, string defaultValue, string tooltip, string label) : base()
+        {
+            Key = key;
+            Value = value;
+            Header = header;
+            Properties = new List<MetaData>
+            {
+                new MetaData("default", defaultValue),
+                new MetaData("label", label),
+                new MetaData("tooltip", tooltip)
+            };
+        }
+    }
+}
