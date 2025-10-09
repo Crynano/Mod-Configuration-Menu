edb54af374d5bb95ac3860417727a86d8cd3847d
Crynano <pa.crysis@gmail.com>
2025-10-09 15:49:43 +0200

    Added StringConfig constructor for IConfigValue

---
+++ src/Implementations/StringConfig.cs
@@ -0,0 +1,14 @@
+﻿using ModConfigMenu.Contracts;
+
+namespace ModConfigMenu.Implementations
+{
+    public class StringConfig : BaseConfig
+    {
+        public StringConfig(string key, string value, string header) : base()
+        {
+            Key = key;
+            Value = value;
+            Header = header;
+        }
+    }
+}
